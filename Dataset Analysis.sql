aa28db0144ac3f88ad79d4e956a938b9bca8c9d0
Mary Isaboke <160160687+Mary-Isaboke@users.noreply.github.com>
2024-06-13 15:28:18 +0300

    Add files via upload

---
+++ Dataset Analysis.sql
@@ -0,0 +1,21 @@
+-- USE netflix_shows;
+
+-- SELECT listed_in AS genre, COUNT(*) AS show_count
+-- FROM netflix_titles
+-- GROUP BY listed_in
+-- ORDER BY show_count DESC;
+
+-- SELECT title, rating
+-- FROM netflix_titles
+-- ORDER BY rating DESC
+-- LIMIT 5;
+
+-- SELECT type, AVG(duration) AS average_duration
+-- FROM netflix_titles
+-- GROUP BY type;
+
+-- SELECT country, COUNT(*) AS show_count
+-- FROM netflix_titles
+-- GROUP BY country
+-- ORDER BY show_count DESC;
+
